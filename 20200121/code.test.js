cec209390cbe36bf492f812cb3c1fa8946e7b7be
kimheechan <kimhecan@gmail.com>
2020-01-21 16:11:38 +0900

    initial commit for JavaScript

---
+++ 20200121/code.test.js
@@ -0,0 +1,33 @@
+/* 마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때,
+ 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요. */
+
+function solution(participant, completion) {
+  const participantObj = participant.reduce((acc, crr) => {
+  acc[crr] ? acc[crr] += 1 : acc[crr] = 1 
+  return acc;
+}, {})
+
+completion.forEach((name,i) => {
+    participantObj[name] -= 1;
+})
+  
+  for(let i in participantObj) {
+      if (participantObj[i] === 1) {
+          return i
+      } 
+  }
+}
+
+
+
+test('solution', () => {
+  expect(solution(["leo", "kiki", "eden"],["eden", "kiki"])).toBe("leo");
+});
+
+test('solution', () => {
+  expect(solution(["marina", "josipa", "nikola", "vinko", "filipa"]	,["josipa", "filipa", "marina", "nikola"])).toBe("vinko");
+})
+
+test('solution', () => {
+  expect(solution(["mislav", "stanko", "mislav", "ana"],["stanko", "ana", "mislav"])).toBe("mislav");
+});
