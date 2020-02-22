80170f3e3c048d1ca6feb0d13d773f3d011ab243
kimheechan <kimhecan@gmail.com>
2020-02-22 23:01:47 +0900

    d

---
+++ 20200222/code.test.js
@@ -0,0 +1,10 @@
+function solution(d, budget) {
+  var answer = 0;
+  return answer;
+}
+
+
+test('solution', () => {
+  expect(solution([1,3,2,5,4], 9)).toBe(3)
+  expect(solution([2,2,3,3], 10)).toBe(4)
+});
