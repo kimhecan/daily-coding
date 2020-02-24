284ddb67c0ba61ce8f8d1fef83988593b3417f8c
kimheechan <kimhecan@gmail.com>
2020-02-24 12:30:47 +0900

    not finding else

--- 20200222/code.test.js
+++ 20200222/code.test.js
@@ -1,10 +1,19 @@
-function solution(d, budget) {
-  var answer = 0;
-  return answer;
+function solution(departList, budget) {
+  departList.sort();
+  let sum = 0;
+
+  for(let i=0; i<departList.length; i++) {
+    sum += departList[i];
+    if (sum > budget) return i
+    if (sum == budget) return i+1
+  }
 }
 
-
 test('solution', () => {
+  expect(solution([1], 1)).toBe(1)
+  expect(solution([2,3,4,5], 1)).toBe(0)
+  expect(solution([1,1,1,1], 3)).toBe(3)
+  expect(solution([1,1,1,1,1], 5)).toBe(5)
   expect(solution([1,3,2,5,4], 9)).toBe(3)
   expect(solution([2,2,3,3], 10)).toBe(4)
 });
--- test.js
+++ test.js
@@ -1,10 +1,17 @@
-process.stdin.setEncoding('utf8');
-process.stdin.on('data', data => {
-    const n = data.split(" ");
-    const a = Number(n[0]), b = Number(n[1]);
-    const row = '*'.repeat(a)
-    for(let i =0; i < b; i++){
-        console.log(row)
-    }
+function solution(departList, budget) {
+  departList.sort();
+  
+  let sum = 0;
+  for(let i=0; i<departList.length; i++) {
+    sum += departList[i];
+    console.log(sum, 'sum', i, 'i');
+    if (sum > budget) return i
+    if (sum == budget) return i+1
+  }
+}
 
-});
+console.log(solution([1,3,2,5,4], 9));
+console.log(solution([2,2,3,3], 10));
+
+
+console.log([1,1, 3,3, 2,2].sort());
