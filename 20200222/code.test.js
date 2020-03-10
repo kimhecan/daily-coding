8c3ab46c597137a39c719cdb03197c4857439757
kimheechan <kimhecan@gmail.com>
2020-03-11 00:22:41 +0900

    edge case를 찾고 싶다.

--- 20200222/code.test.js
+++ 20200222/code.test.js
@@ -6,14 +6,25 @@ function solution(departList, budget) {
     sum += departList[i];
     if (sum > budget) return i
     if (sum == budget) return i+1
+    if (i == departList.length - 1) return departList.length 
   }
 }
 
+
+// function solution(d, budget) {
+//   d.sort((a, b) => a - b);
+
+//   while (d.reduce((a, b) => (a + b), 0) > budget) d.pop();
+
+//   return d.length;
+// }
+
+
 test('solution', () => {
-  expect(solution([1], 1)).toBe(1)
+  expect(solution([1,1], 3)).toBe(2)
   expect(solution([2,3,4,5], 1)).toBe(0)
   expect(solution([1,1,1,1], 3)).toBe(3)
-  expect(solution([1,1,1,1,1], 5)).toBe(5)
+  expect(solution([1,1,1,1,1], 4)).toBe(4)
   expect(solution([1,3,2,5,4], 9)).toBe(3)
-  expect(solution([2,2,3,3], 10)).toBe(4)
+  expect(solution([2,2,3,3], 13)).toBe(4)
 });
