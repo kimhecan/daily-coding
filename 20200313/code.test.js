d825da719478c3c58f390c259b8890c20188b30f
kimhecan <kimhecan@gmail.com>
2020-03-13 14:43:01 +0900

    ..

---
+++ 20200313/code.test.js
@@ -0,0 +1,13 @@
+function solution(n, stages) {
+  let percentage = []
+
+  return answer;
+}
+
+
+
+
+test('solution', () => {
+  expect(solution(5, [2, 1, 2, 6, 2, 4, 3, 3])).toBe([3,4,2,1,5]);
+  expect(solution(4, [4,4,4,4,4])).toBe([4,1,2,3]);
+})
