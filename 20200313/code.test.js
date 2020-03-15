f234d5456c0dab049dd1785d9109990c6b4fafc8
kimhecan <kimhecan@gmail.com>
2020-03-15 23:21:37 +0900

    ..

--- 20200313/code.test.js
+++ 20200313/code.test.js
@@ -1,8 +1,29 @@
 function solution(n, stages) {
   let percentage = []
+  let unsortPercentage = []
+  let preDenominator = 0;
+  let stagesLength = stages.length
 
+  // 각 스테이지의 실패율 구하기
+  for(let i=1; i<=n; i++) {
+    let afterDenominator = 0;
+    stages.forEach(v => v == i ? afterDenominator++ : null );
+    stagesLength -= preDenominator;
+    percentage[i-1] = afterDenominator / stagesLength;
+    unsortPercentage[i-1] = afterDenominator / stagesLength;
+    preDenominator = afterDenominator;
+  }
+  percentage.sort((a, b) => b-a);
+
+  let answer = [];
+  // 실패율에 따른 정렬과 인덱스 저장
+  percentage.forEach((v, i) => {
+    answer[i] = unsortPercentage.indexOf(percentage[i])+1;
+    unsortPercentage.splice(unsortPercentage.indexOf(percentage[i]),1,4);
+  })
   return answer;
 }
+
 
 
 
--- test.js
+++ test.js
@@ -1,11 +1,29 @@
-function solution(n, arr1, arr2) {
-  let deArray1 = arr1.map(v => parseInt(v.toString(2),10));
-  let deArray2 = arr2.map(v => parseInt(v.toString(2),10));
+function solution(n, stages) {
+  let percentage = []
+  let unsortPercentage = []
+  let preDenominator = 0;
+  let stagesLength = stages.length
 
-  let sumArray = deArray1.map((v, i) => String(v + deArray2[i]).replace(/2|1|0/g, a=> +a ?  '#' : ' '));
-  let answer = sumArray.map((v, i) => ' '.repeat(n - v.length) + v);  
-  
-  return answer
+  for(let i=1; i<=n; i++) {
+    let afterDenominator = 0;
+    stages.forEach(v => v == i ? afterDenominator++ : null );
+    stagesLength -= preDenominator;
+    percentage[i-1] = afterDenominator / stagesLength;
+    unsortPercentage[i-1] = afterDenominator / stagesLength;
+    preDenominator = afterDenominator;
+  }
+  percentage.sort((a, b) => b-a);
+
+  let answer = [];
+  percentage.forEach((v, i) => {
+    answer[i] = unsortPercentage.indexOf(percentage[i])+1;
+    unsortPercentage.splice(unsortPercentage.indexOf(percentage[i]),1,4);
+  })
+  return answer;
 }
 
-solution(6,[46, 33, 33 ,22, 31, 50],[27 ,56, 19, 14, 14, 10])
+console.log(solution(5,[2,1,2,6,2,4,3,3]));
+console.log(solution(4,[4,4,4,4,4]));
+console.log(solution(1,[1,1,1,1,1,1]));
+console.log(solution(2,[1,3]));
+console.log(solution(10,[1,1,1,2,2,3,3,3,7,9]));
