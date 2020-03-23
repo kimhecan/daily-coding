2670820bbc7f130c68090cd0ba74331ca6daa237
kimhecan <kimhecan@gmail.com>
2020-03-23 23:39:06 +0900

    흑

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-import re
+code = """
+k   k   I   M      M     H  H     EEEEEE   EEEEEE          C  H  H      A      N    N
+k k     I   M M   MM     H  H     E        E             C    H  H     A A     N N  N
+kk      I   M   M  M     HHHH     EEEEEE   EEEEEE       C     HHHH    AAAAA    N  N N
+k k     I   M      M     H  H     E        E             C    H  H   A     A   N   NN
+k   k   I   M      M     H  H     EEEEEE   EEEEEE          C  H  H  A       A  N    N
+"""
 
-a = re.compile('\d').findall('ddd')
-print(len(a))
+print(code)
+
